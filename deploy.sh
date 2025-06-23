fe6976f64b9c42f0462db7edad23fdca3ab8740a
gitomarisrael <omarisraelmartinezsilva@gmail.com>
2025-06-23 13:43:31 -0600

    file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
+BRANCH=?1
+
+if [ "$BRANCH" == "main"]; then 
+    DEPLOY_PATH="/home/omarisraelmartinezsilva/practica-git-actions-azure"
+else
+    DEPLOY_PATH="/home/omarisraelmartinezsilva/stage"
+fi
+
 ssh -o StrictHostKeyChecking=no omarisraelmartinezsilva@34.173.95.68 <<HTML
 
-    cd /home/omarisraelmartinezsilva/practica-git-actions-azure
-    
-    git pull --rebase origin main
+    cd $DEPLOY_PATH
+
+    git pull --rebase origin $BRANCHX
 
 HTML
