c5b48cd820910edd9ec9a6d3ffccc54ce220e1bd
gitomarisrael <omarisraelmartinezsilva@gmail.com>
2025-06-23 13:08:07 -0600

    wip

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+ssh -o StrictHostKeyChecking=no omarisraelmartinezsilva@34.173.95.68 <<HTML
+
+    cd /home/omarisraelmartinezsilva/practica-git-actions-azure
+    
+    git pull --rebase origin main
+
+HTML
