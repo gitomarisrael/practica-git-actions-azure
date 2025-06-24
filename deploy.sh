6bc83ca5ef3fcfe961c01f05e29387c7eb5054fd
gitomarisrael <omarisraelmartinezsilva@gmail.com>
2025-06-24 09:47:27 -0600

    prod

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 BRANCH=$1
 
-if [ "$BRANCH" == "main"]; then 
+if [ "$BRANCH" == "main" ]; then 
     DEPLOY_PATH="/home/omarisraelmartinezsilva/practica-git-actions-azure"
 else
     DEPLOY_PATH="/home/omarisraelmartinezsilva/stage"
