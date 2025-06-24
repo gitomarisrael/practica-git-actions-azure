2480914f156d31d8ed5cee1f3a299957d26dcf94
gitomarisrael <omarisraelmartinezsilva@gmail.com>
2025-06-24 13:28:42 -0600

    main

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ else
     DEPLOY_PATH="/home/omarisraelmartinezsilva/stage"
 fi
 
-ssh -o StrictHostKeyChecking=no omarisraelmartinezsilva@34.173.95.68 <<HTML
+ssh -o StrictHostKeyChecking=no omarisraelmartinezsilva@34.171.144.130 <<HTML
 
     cd $DEPLOY_PATH
 
